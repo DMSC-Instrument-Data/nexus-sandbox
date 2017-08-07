e5cfcaffdbe68d0786b862f5781bee42d0e82a77
Simon Heybrock <simon.heybrock@esss.se>
2017-08-07 13:24:52 +0200

    Testing. WIP.

--- src/load_parallel.cpp
+++ src/load_parallel.cpp
@@ -15,19 +15,44 @@ int main(int argc, char **argv) {
   // /home/simon/mantid/nexus/load-performance/sample-files/gzip-level6.nxs
   H5::H5File file(argv[1], H5F_ACC_RDONLY);
 
-  std::vector<int> banks = {22,  23,  24,  42,  43,  44,  62,  63,
-                            64,  82,  83,  84,  102, 103, 104, 105,
-                            106, 123, 124, 143, 144, 164, 184};
+  //std::vector<int> banks = {22,  23,  24,  42,  43,  44,  62,  63,
+  //                          64,  82,  83,  84,  102, 103, 104, 105,
+  //                          106, 123, 124, 143, 144, 164, 184};
+
+  std::vector<int> banks = {28};
 
   std::string prefix("entry/bank");
   std::string suffix("_events/");
-  std::string name = prefix + std::to_string(banks[comm.rank()]) + suffix;
+  std::string name = prefix + std::to_string(banks[0]) + suffix;
+  //std::string name = prefix + std::to_string(banks[comm.rank()]) + suffix;
 
   const auto start = std::chrono::system_clock::now();
+  size_t size = 0;
 
   comm.barrier();
+
+  {
+  hsize_t count = 2048*1024;
+  hsize_t start = count * comm.rank();
+  while (true) {
+    const auto data = read<int32_t>(file, name + "event_id", start, count);
+    if(data.empty())
+      break;
+    start += count * comm.size();
+    size += data.size() * sizeof(int32_t);
+  }
+  }
+  /*
   auto id = read<int32_t>(file, name + "event_id");
   auto tof = read<float>(file, name + "event_time_offset");
+  {
+  hsize_t start = 0;
+  hsize_t count = 2048*1;
+  while (read<int32_t>(file, name + "event_id", start, count).size() == count) {
+    start += count;
+  }
+  }
+  */
   comm.barrier();
 
   const auto end = std::chrono::system_clock::now();
@@ -37,7 +62,6 @@ int main(int argc, char **argv) {
                        elapsed).count() /
                    (1000000000);
 
-  const auto size = id.size() * sizeof(int32_t) + tof.size() * sizeof(float);
   size_t sum;
   boost::mpi::reduce(comm, size, sum, std::plus<size_t>(), 0);
   if (comm.rank() == 0)
