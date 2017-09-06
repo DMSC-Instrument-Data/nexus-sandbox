ea88c65b702e62b30ba30595dc52f6e257edca33
Simon Heybrock <simon.heybrock@esss.se>
2017-09-06 11:20:05 +0200

    WIP full file read and workspace insertion.

--- src/NXevent_data_main.cpp
+++ src/NXevent_data_main.cpp
@@ -12,6 +12,12 @@ struct Event {
   int64_t pulse_time;
 };
 
+struct LoadRange {
+  int bank;
+  hsize_t start;
+  hsize_t count;
+};
+
 // In-place translation from event_id to GlobalSpectrumIndex. Note that
 // GlobalSpectrumIndex is actually 64 bit, but if the event_id is 32 bit it is
 // guaranteed that the index also fits into 32 bit (unless we have scanning,
@@ -26,19 +32,40 @@ void event_id_to_GlobalSpectrumIndex(const int32_t event_id_offset,
 }
 
 std::vector<Event>
-build_event_vector(const std::vector<int32_t> &event_index,
+build_event_vector(const LoadRange &range,
+                   const std::vector<int32_t> &event_index,
                    const std::vector<int64_t> &event_time_zero,
                    const std::vector<int32_t> &event_global_spectrum_index,
                    const std::vector<int32_t> &event_time_offset) {
-  std::vector<Event> events(event_time_offset.size());
+  size_t start_pulse = 0;
+  size_t end_pulse = 0;
   for (size_t pulse = 0; pulse < event_index.size(); ++pulse) {
-    size_t base = event_index[pulse];
     size_t count =
         (pulse != event_index.size() - 1 ? event_index[pulse + 1]
                                          : event_time_offset.size()) -
         event_index[pulse];
-    for (size_t offset = 0; offset < count; ++offset) {
-      size_t i = base + offset;
+    if (range.start >= event_index[pulse] &&
+        range.start < event_index[pulse] + count)
+      start_pulse = pulse;
+    if (range.start + range.count > event_index[pulse] &&
+        range.start + range.count <= event_index[pulse] + count)
+      end_pulse = pulse + 1;
+  }
+  // event_index should have offset range.start
+  // count 
+  std::vector<Event> events(event_time_offset.size());
+  for (size_t pulse = start_pulse; pulse < end_pulse; ++pulse) {
+    size_t start =
+        std::max(range.start, static_cast<hsize_t>(event_index[pulse])) -
+        event_index[pulse];
+    size_t end =
+        std::min(range.start + range.count,
+                 static_cast<hsize_t>(pulse != event_index.size() - 1
+                                          ? event_index[pulse + 1]
+                                          : event_time_offset.size())) -
+        event_index[pulse];
+
+    for (size_t i = start; i < end; ++i) {
       events[i] = {event_global_spectrum_index[i], event_time_offset[i],
                    event_time_zero[pulse]};
     }
@@ -107,6 +134,67 @@ void append_to_workspace(const int nrank,
   }
 }
 
+
+std::vector<LoadRange> determineLoadRanges(int nrank, int rank, H5::H5File &file) {
+  // Work balancing should take into account total number of events and then
+  // chunk things up for MPI
+  size_t n_events{0};
+  std::vector<size_t> bank_sizes;
+  for (int i = 0; i < 7; ++i) {
+    H5::DataSet dataset = file.openDataSet("entry/instrument/events-" +
+                                           std::to_string(i) + "/event_id");
+    H5::DataSpace dataSpace = dataset.getSpace();
+    bank_sizes.push_back(dataSpace.getSelectNpoints());
+    n_events += bank_sizes.back();
+  }
+  printf("total events %lu\n", n_events);
+
+  std::vector<size_t> bank_ends(bank_sizes);
+  for (size_t i = 1; i < bank_ends.size(); ++i)
+    bank_ends[i] += bank_ends[i - 1];
+  std::vector<size_t> bank_begins(bank_ends);
+  for (size_t i = 0; i < bank_begins.size(); ++i)
+    bank_begins[i] -= bank_sizes[i];
+
+  size_t offset = n_events / nrank;
+  size_t start = offset * rank;
+  size_t last = offset * (rank + 1) - 1;
+  if (rank == nrank - 1)
+    last += n_events - (offset * nrank);
+
+  auto it = std::lower_bound(bank_ends.begin(),
+                                   bank_ends.end(), start);
+  auto last_it = std::upper_bound(bank_ends.begin(), bank_ends.end(), last);
+  std::vector<LoadRange> ranges;
+  while(it <= last_it) {
+    int bank =
+        static_cast<int>(std::distance(bank_ends.begin(), it));
+    hsize_t range_start =
+        std::max(start, bank_begins[bank]) - bank_begins[bank];
+
+    hsize_t range_end = std::min(last + 1, bank_ends[bank]) - bank_begins[bank];
+    ranges.push_back(LoadRange{bank, range_start, range_end - range_start});
+    ++it;
+  }
+  return ranges;
+
+
+
+
+
+  /*
+  size_t current = 0;
+  for(size_t bank=0; bank<bank_sizes.size(); ++bank) {
+    const auto bank_size = bank_sizes) {
+    current += bank_size;
+    if(first < current) {
+      if(last < current)
+        range.push_back(LoadRange(
+    }
+  }
+  */
+}
+
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
   int rank;
@@ -117,47 +205,48 @@ int main(int argc, char **argv) {
   H5::H5File file(argv[1], H5F_ACC_RDONLY);
   size_t n_events = 0;
   Timer timer;
-  /*
-  // Work balancing should take into account total number of events and then
-  // chunk things up for MPI
-  for (int i = 0; i < 7; ++i) {
-    H5::DataSet dataset = file.openDataSet("entry/instrument/events-" +
-                                           std::to_string(i) + "/event_id");
-    H5::DataSpace dataSpace = dataset.getSpace();
-    n_events += dataSpace.getSelectNpoints();
-  }
-  printf("counted %lu events total %lf seconds\n", n_events,
-         timer.checkpoint());
-         */
-  NXEventDataLoader loader(file,
-                           "entry/instrument/events-" + std::to_string(rank));
-  // TODO avoid reallocating many times. Reuse buffer (double buffering when
-  // threaded?)
-  const size_t n_event = loader.numberOfEvents();
-  const auto &event_index = loader.eventIndex();
-  const auto &event_time_zero = loader.eventTimeZero();
-  auto event_id = loader.readEventID(0, n_event);
-  const auto &event_time_offset = loader.readEventTimeOffset(0, n_event);
-  timer.checkpoint();
 
-  event_id_to_GlobalSpectrumIndex(100000*rank, event_id);
+  //for(const auto &range : ranges)
+  //  printf("%d %d %llu %llu\n", rank, range.bank, range.start, range.count);
+
+  int num_loaded_banks = 7;
+  int pixels_per_bank = 10000;
+  std::vector<std::vector<MantidEvent>> workspace(
+      (num_loaded_banks * pixels_per_bank) / nrank);
+  std::vector<Event> events;
+  for (const auto &range : determineLoadRanges(nrank, rank, file)) {
+    NXEventDataLoader loader(file, "entry/instrument/events-" +
+                                       std::to_string(range.bank));
+    // TODO avoid reallocating many times. Reuse buffer (double buffering when
+    // threaded?)
+    const size_t n_event = loader.numberOfEvents();
+    const auto &event_index = loader.eventIndex();
+    const auto &event_time_zero = loader.eventTimeZero();
+    auto event_id = loader.readEventID(range.start, range.count);
+    const auto &event_time_offset =
+        loader.readEventTimeOffset(range.start, range.count);
+    timer.checkpoint();
+
+    event_id_to_GlobalSpectrumIndex(100000 * range.bank, event_id);
+    timer.checkpoint();
+    // event_id is now actually event_global_spectrum_index
+    const auto &events_this_range = build_event_vector(
+        range, event_index, event_time_zero, event_id, event_time_offset);
+    events.insert(events.end(), events_this_range.begin(),
+                  events_this_range.end());
+  }
   timer.checkpoint();
-  // event_id is now actually event_global_spectrum_index
-  const auto &events = build_event_vector(event_index, event_time_zero, event_id, event_time_offset);
-  timer.checkpoint();
+  printf("%lu\n", events.size());
   const auto &events_for_ranks = split_for_ranks(nrank, events);
   timer.checkpoint();
   const auto &events_for_this_rank = redistribute_data(events_for_ranks);
   timer.checkpoint();
-  int num_loaded_banks = nrank;
-  int pixels_per_bank = 10000;
-  std::vector<std::vector<MantidEvent>> workspace(
-      (num_loaded_banks * pixels_per_bank) / nrank);
   append_to_workspace(nrank, workspace, events_for_this_rank);
   timer.checkpoint();
 
   if (rank == 0) {
     printf("HDF5-load id-to-index event-vec split-ranks MPI make-worksapce\n");
+    printf("xxx ");
     for (const auto seconds : timer.deltas())
       printf("%lf ", seconds);
     printf("\n");
