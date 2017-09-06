c4411da9b196b8d6a2718244879e4ff2015a5420
Simon Heybrock <simon.heybrock@esss.se>
2017-09-06 15:11:48 +0200

    Reuse vectors where possible.

--- src/NXevent_data_main.cpp
+++ src/NXevent_data_main.cpp
@@ -31,12 +31,11 @@ void event_id_to_GlobalSpectrumIndex(const int32_t event_id_offset,
                 [&](int32_t &i) { i -= event_id_offset; });
 }
 
-std::vector<Event>
-build_event_vector(const LoadRange &range,
-                   const std::vector<int32_t> &event_index,
-                   const std::vector<int64_t> &event_time_zero,
-                   const std::vector<int32_t> &event_global_spectrum_index,
-                   const std::vector<int32_t> &event_time_offset) {
+void build_event_vector(std::vector<Event> &events, const LoadRange &range,
+                        const std::vector<int32_t> &event_index,
+                        const std::vector<int64_t> &event_time_zero,
+                        const std::vector<int32_t> &event_global_spectrum_index,
+                        const std::vector<int32_t> &event_time_offset) {
   size_t start_pulse = 0;
   size_t end_pulse = 0;
   for (size_t pulse = 0; pulse < event_index.size(); ++pulse) {
@@ -53,7 +52,7 @@ build_event_vector(const LoadRange &range,
   }
   // event_index should have offset range.start
   // count 
-  std::vector<Event> events(event_time_offset.size());
+  events.resize(event_time_offset.size());
   for (size_t pulse = start_pulse; pulse < end_pulse; ++pulse) {
     size_t start =
         std::max(range.start, static_cast<hsize_t>(event_index[pulse])) -
@@ -70,25 +69,22 @@ build_event_vector(const LoadRange &range,
                    event_time_zero[pulse]};
     }
   }
-  return events;
 }
 
 template <class T>
-std::vector<std::vector<T>> split_for_ranks(const int nrank,
-                                            const std::vector<T> &data) {
-  std::vector<std::vector<T>> rank_data(nrank);
-  for(auto &item : rank_data)
-    item.reserve(150000000/nrank);
+void split_for_ranks(std::vector<std::vector<T>> &rank_data, const int nrank,
+                     const std::vector<T> &data) {
+  for (auto &item : rank_data)
+    item.clear();
   for (const auto &item : data) {
     int rank = item.index % nrank;
     rank_data[rank].push_back(item);
   }
-  return rank_data;
 }
 
 template <class T>
-std::vector<T>
-redistribute_data(const std::vector<std::vector<T>> &data) {
+void redistribute_data(std::vector<T> &result,
+                       const std::vector<std::vector<T>> &data) {
   std::vector<size_t> sizes(data.size());
   std::transform(data.cbegin(), data.cend(), sizes.begin(),
                  [](const std::vector<T> &vec) { return vec.size(); });
@@ -105,17 +101,20 @@ redistribute_data(const std::vector<std::vector<T>> &data) {
   }
 
   size_t total_size = std::accumulate(rec_sizes.begin(), rec_sizes.end(), 0);
-  std::vector<T> result(total_size);
+  result.resize(total_size);
   size_t offset = 0;
   for (int rank = 0; rank < data.size(); ++rank) {
     int tag = 0;
     MPI_Recv(result.data() + offset, rec_sizes[rank] * sizeof(T), MPI_CHAR,
              rank, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     offset += rec_sizes[rank];
+    // TODO
+    // 1. everyone is receiving data from rank 0 first, slowing things down?
+    // 2. do work between sending and receiving (next range, or insert into
+    // workspace after first data was received?).
   }
 
   MPI_Waitall(data.size(), send_requests.data(), MPI_STATUSES_IGNORE);
-  return result;
 }
 
 struct MantidEvent {
@@ -227,7 +226,6 @@ int main(int argc, char **argv) {
 
   H5::H5File file(argv[1], H5F_ACC_RDONLY);
   size_t n_events = 0;
-  Timer timer;
 
   //for(const auto &range : ranges)
   //  printf("%d %d %llu %llu\n", rank, range.bank, range.start, range.count);
@@ -235,8 +233,13 @@ int main(int argc, char **argv) {
   int num_loaded_banks = 7;
   int pixels_per_bank = 10000;
   std::vector<std::vector<MantidEvent>> workspace(
-      (num_loaded_banks * pixels_per_bank) / nrank);
+      (num_loaded_banks * pixels_per_bank) / nrank + 1);
+  std::vector<Event> events;
+  std::vector<std::vector<Event>> events_for_ranks(nrank);
+  std::vector<Event> &events_for_this_rank = events;
+  std::vector<double> deltas(6, 0.0);
   for (const auto &range : determineChunkedLoadRanges(nrank, rank, file)) {
+    Timer timer;
     NXEventDataLoader loader(file, "entry/instrument/events-" +
                                        std::to_string(range.bank));
     // TODO avoid reallocating many times. Reuse buffer (double buffering when
@@ -252,21 +255,24 @@ int main(int argc, char **argv) {
     event_id_to_GlobalSpectrumIndex(100000 * range.bank, event_id);
     timer.checkpoint();
     // event_id is now actually event_global_spectrum_index
-    const auto &events = build_event_vector(range, event_index, event_time_zero,
+    build_event_vector(events, range, event_index, event_time_zero,
                                             event_id, event_time_offset);
     timer.checkpoint();
-    const auto &events_for_ranks = split_for_ranks(nrank, events);
+    split_for_ranks(events_for_ranks , nrank, events);
     timer.checkpoint();
-    const auto &events_for_this_rank = redistribute_data(events_for_ranks);
+    redistribute_data(events_for_this_rank, events_for_ranks);
     timer.checkpoint();
     append_to_workspace(nrank, workspace, events_for_this_rank);
     timer.checkpoint();
+    const auto seconds = timer.deltas();
+    for(size_t i=0; i<deltas.size(); ++i)
+      deltas[i] += seconds[i];
   }
 
   if (rank == 0) {
     printf("HDF5-load id-to-index event-vec split-ranks MPI make-worksapce\n");
     printf("xxx ");
-    for (const auto seconds : timer.deltas())
+    for (const auto seconds : deltas)
       printf("%lf ", seconds);
     printf("\n");
   }
