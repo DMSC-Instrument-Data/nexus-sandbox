40d8e8ad498568ac1029936079e3c8abd0bfd3ea
Simon Heybrock <simon.heybrock@esss.se>
2018-06-20 08:02:18 +0200

    Better help in grow.py.

--- scripts/grow.py
+++ scripts/grow.py
@@ -6,15 +6,18 @@ import numpy
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-f", "--filename", type=str, default='', help='File to grow. Modified in place.')
-parser.add_argument("-s", "--scale", type=float, default=2, help='Scale factor to multiply the number of events.')
+parser.add_argument("-s", "--scale", type=float, default=2, help='Scale factor to multiply the number of events. If the factor is greater than 1 it is rounded to the nearest integer and the number of events per pulse is increased by that factor. If the factor is less than 1 the number of pulses is reduced accordingly, keeping the number of events per pulse as in the original file.')
 parser.add_argument("-r", "--root", type=str, default='entry', help='Name of the root entry containing the NXevent_data entries.')
 args = parser.parse_args()
 
 def grow(filename):
     with h5py.File(filename, 'a') as f:
-        scale = args.scale
-        if scale < 1:
+        if args.scale < 1:
+            scale = args.scale
             print('Warning: Reducing event count, but chunking is left untouched. If the chunk size is large the resulting file may actually not shrink. If that is the case, use something like `h5repack -i {0}.nxs -o {0}-rechunked.nxs -l CHUNK=N` to produce a smaller file.'.format(filename[:-4]))
+        else:
+            scale = int(round(args.scale))
+
         for bank, event_data in f[args.root].items():
             if not bank.endswith('_events'):
                 continue
